42c21677afc6e64d17ae663d1c5c8abf347772ea
Brayan Araneda <bjac5002@gmail.com>
2025-03-06 18:50:38 -0300

    Agregando variables y array

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const primeraPantalla = document.getElementById('inicio')
+
+const maria = document.getElementById('maria')
+const javier = document.getElementById('javier')
+const sofia = document.getElementById('sofia')
+const andres = document.getElementById('andres')
+
+var personajes = [
+    { nombre: "Maria", cuenta: "Corrinte", dinero: 684, clave: 7154 },
+    { nombre: "Javier", cuenta: "Vista", dinero: 978, clave: 8520 },
+    { nombre: "Sofía", cuenta: "Ahorro", dinero: 598, clave: 3817 },
+    { nombre: "Andrés", cuenta: "Débito", dinero: 749, clave: 1863 },
+]
