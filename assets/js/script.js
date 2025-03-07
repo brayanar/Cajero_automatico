0cdbb0096194e3765d820d2f72316f8a5558e72e
Brayan Araneda <bjac5002@gmail.com>
2025-03-07 18:02:11 -0300

    Avances en guardado de clave de personaje y cambios de pantalla

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,5 @@
 const primeraPantalla = document.getElementById('inicio')
-
-const maria = document.querySelector("#maria")
-maria.addEventListener("click",seleccion)
-const javier = document.querySelector("#javier")
-javier.addEventListener("click",seleccion)
-const sofia = document.querySelector("#sofia")
-sofia.addEventListener("click",seleccion)
-const andres = document.querySelector("#andres")
-andres.addEventListener("click",seleccion)
+const formulario = document.getElementById('form')
 
 var personajes = [
     { nombre: "Maria", cuenta: "Corrinte", dinero: 684, clave: 7154 },
@@ -15,3 +7,37 @@ var personajes = [
     { nombre: "Sofía", cuenta: "Ahorro", dinero: 598, clave: 3817 },
     { nombre: "Andrés", cuenta: "Débito", dinero: 749, clave: 1863 },
 ]
+
+
+function claveMaria() {
+    let i = personajes[0].clave
+    primeraPantalla.style.display = "none"
+    formulario.style.display = "unset"
+    console.log(i)
+}
+
+function claveJavier() {
+    let i = personajes[1].clave
+    primeraPantalla.style.display = "none"
+    console.log(i)
+}
+function claveSofia() {
+    let i = personajes[2].clave
+    primeraPantalla.style.display = "none"
+    console.log(i)
+}
+function claveAndres() {
+    let i = personajes[3].clave
+    primeraPantalla.style.display = "none"
+    console.log(i)
+}
+
+function clave() {
+    const claveIngresada = document.getElementById("pwd")
+    console.log(claveIngresada)
+}
+
+formulario.addEventListener('submit', function() {
+    let pwd = document.getElementById('pwd').value
+    console.log(pwd)
+})
