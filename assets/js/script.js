d75e45bfad7ae98bba2f377f7573041900a885c8
Brayan Araneda <bjac5002@gmail.com>
2025-03-14 09:45:40 -0300

    Adicion de pantalla de retiro de otro monto y logica del retiro en javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,10 @@ const primeraPantalla = document.getElementById('inicio')
 const password = document.getElementById('form1')
 const pantallaCuentas = document.getElementById('ctas')
 const pantallaMontos = document.getElementById('montosSugeridos')
+const pantallaOtroMonto = document.getElementById('pantallaMontoD')
+const pantallaConfirmarMonto = document.getElementById('confirmar1')
+var saldoPersonaje
+var valor
 
 var personajes = [
     { nombre: "Emma", cuenta: "corriente", dinero: 684, clave: 7154 },
@@ -18,6 +22,7 @@ function seleccionarPersonaje(indice) {
     personajeSeleccionado = personajes[indice]
     primeraPantalla.style.display = "none"
     password.style.display = "block"
+    saldoPersonaje = personajes[indice].dinero
     console.log(personajeSeleccionado.clave)
 }
 
@@ -25,7 +30,7 @@ password.addEventListener('submit', function () {
     let pwd = document.getElementById('pwd').value
     console.log(pwd)
 
-    if(pwd == personajeSeleccionado.clave) {
+    if (pwd == personajeSeleccionado.clave) {
         alert("Clave correcta")
         password.style.display = "none"
         pantallaCuentas.style.display = "block"
@@ -42,7 +47,7 @@ function cuentaUsuario(tipoCuenta) {
     if (cuentaUsuarioSeleccinada === personajeSeleccionado.cuenta) {
         alert("Correcto")
         montoUsuario()
-        pantallaMontos.style.display="block"
+        pantallaMontos.style.display = "block"
         pantallaCuentas.style.display = "none"
     } else {
         alert("No")
@@ -50,15 +55,41 @@ function cuentaUsuario(tipoCuenta) {
 }
 
 function consultaSaldo() {
-    pantallaCuentas.style.display="none"
+    pantallaCuentas.style.display = "none"
     let saldo = document.getElementById("saldo")
     saldo.textContent = personajeSeleccionado.nombre + " tiene " + personajeSeleccionado.dinero + " dolares de saldo"
-    saldo.style.display="block"
+    saldo.style.display = "block"
 }
 
-let montoUsuarioSeleccionado ;
+let montoUsuarioSeleccionado;
 
-function montoUsuario (indice) {
+function montoUsuario(indice) {
     montoUsuarioSeleccionado = indice
     console.log(indice)
+    console.log(saldoPersonaje)
+}
+
+function otroMonto (){
+    pantallaMontos.style.display="none"
+    enviarValor()
+}
+
+function enviarValor() {
+    pantallaOtroMonto.style.display="block"
+    valor = document.getElementById("valorInput").value;
+
+    if (valor.trim() !== "") {
+        document.getElementById("valorEnviado").textContent = "Confirmar el retiro de: " + valor + " dolares";
+        document.getElementById("valorEnviado").style.display = "block";
+        document.getElementById("valorInput").value = "";
+        console.log(valor)
+        pantallaConfirmarMonto.style.display="block"
+    } else {
+        alert("Por favor, ingrese un valor.");
+    }
+}
+
+function retirarMonto() {
+    saldoPersonaje = saldoPersonaje-valor
+    console.log(saldoPersonaje)
 }
